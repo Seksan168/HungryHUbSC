213a2b7838552ef517f3b28d08799b449c9b4492
Seksan168 <s6530613003@phuket.psu.ac.th>
2024-02-21 21:15:23 +0700

    Unupload data

--- Skyscanner.js
+++ Skyscanner.js
@@ -1,7 +1,9 @@
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { franc } from "franc";
-import LanguageDetect from "languagedetect";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
 
 puppeteer.use(StealthPlugin());
 
@@ -13,6 +15,7 @@ async function scrollToBottom(page) {
         }
     });
 }
+
 (async () => {
     try {
         const browser = await puppeteer.launch({
@@ -28,6 +31,8 @@ async function scrollToBottom(page) {
         const buttonReviewSelector = '#app-root > div.MainContent_MainContent__section__Y2FjZ.SEOPageLayout_SEOPageLayout__hotelInformation__MDMxY > div > div.DesktopLayout_DesktopLayout__reviewWrap__ZTkwZ > div > div > section';
         await page.click(buttonReviewSelector);
         await scrollToBottom(page);
+        let isButtonDisabled = false;
+        let retries = 3; // Number of times to retry clicking the button
         
         let allQuotes = [];
            
@@ -77,6 +82,68 @@ async function scrollToBottom(page) {
                             hotel: hotelName,
                             review: reviewText,
                             // language: language,
+                            rating: ratingText.replaceAll("\n",""),
+                            date: dateText,
+                            reference: "Skyscanner",
+                        });
+                    });
+                    
+                    
+                    return boxCompress;
+                    
+                });
+                
+                allQuotes = allQuotes.concat(quotes);
+        while (!isButtonDisabled && retries > 0) {
+            try {
+                const nextButtonselector = 'nav.BpkPagination_bpk-pagination__N2VhM > button:nth-child(3)';
+                let allQuotes = [];
+           
+        
+                const quotes = await page.evaluate(() => {
+                    const nameElement = document.querySelector('.DesktopLayout_DesktopLayout__nameStars__MGE5Y > h1');
+                    const hotelName = nameElement ? nameElement.textContent.trim() : null;
+                    
+                    const boxElement = document.querySelectorAll("div.ReviewCardItem_ReviewCardItem__YTU2Y ");
+                    
+                    const boxCompress = [];
+        
+                    boxElement.forEach(async element => {
+                        const reviewEl = element.querySelector("div.ReviewCardItem_ReviewCardItem__rightSection__YzZkZ > div.ReviewCardItem_ReviewCardItem__reviewContent__OWU1N > span");
+                        const reviewText = reviewEl ? reviewEl.innerText.trim() : "Review not found";
+                        
+                        const ratingEl = element.querySelector("span.BpkText_bpk-text__ZjI3M.BpkText_bpk-text--label-1__MWI4N.BpkRating_bpk-rating__value__YzhiN");
+                        const ratingText = ratingEl ? ratingEl.innerText.split('/')[0].trim() : "Rating not found";
+                        
+                        const dateEl = element.querySelector("div.ReviewCardItem_ReviewCardItem__guestInfo__YmE1Y > p.BpkText_bpk-text__ZjI3M.BpkText_bpk-text--caption__NzU1O.ReviewCardItem_ReviewCardItem__info__YTg5Z");
+                        const dateText = dateEl ? formatDate(dateEl.innerText) : "Date not found";
+                        console.log('Check Lang');
+                        // const language = franc(reviewText, { only: ['tha'] });
+                        console.log('Check Lang f');
+                    
+                    
+                        function formatDate(dateString) {
+                            const datePart = dateString.split(':')[1].trim();
+                            
+                            // Check if the date string matches the expected format
+                            const dateRegex = /^(\d{1,2}) (\S+) (\d{4})$/;
+                            if (!dateRegex.test(datePart)) {
+                                // If the date format doesn't match, return a default value
+                                return "Invalid date format";
+                            }
+                            
+                            // Parse the date and return it in YYYY/MM/DD format
+                            const [, day, month, year] = datePart.match(dateRegex);
+                            const monthNumber = {
+                                'ม.ค.': '01', 'ก.พ.': '02', 'มี.ค.': '03', 'เม.ย.': '04', 'พ.ค.': '05', 'มิ.ย.': '06',
+                                'ก.ค.': '07', 'ส.ค.': '08', 'ก.ย.': '09', 'ต.ค.': '10', 'พ.ย.': '11', 'ธ.ค.': '12'
+                            }[month];
+                            return `${year}/${monthNumber}/${day}`;
+                        }
+                        
+                        boxCompress.push({
+                            hotel: hotelName,
+                            review: reviewText.replaceAll("\n",""),
                             rating: ratingText,
                             date: dateText,
                             reference: "Skyscanner",
@@ -89,15 +156,110 @@ async function scrollToBottom(page) {
                 });
                 
                 allQuotes = allQuotes.concat(quotes);
+                
+                
+           
+        
         console.log('Data scraping complete.');
         console.log('Number of quotes:', allQuotes.length);
         allQuotes.forEach(obj => {
             const language = franc(obj.review);
             obj.language = language;
-            console.log(obj);
+            // console.log(obj);
         });
-        
+                
+                // Wait for the button to be available and click it. Increase the timeout if necessary.
+                await page.waitForSelector(nextButtonselector, { timeout: 60000 }); // 60 seconds
+                await page.click(nextButtonselector);
+    
+                // Wait for some time to allow for the state change to occur
+                await page.waitForTimeout(5000); // Adjust the time as needed
+    
+                // Check if the button is disabled
+                isButtonDisabled = await page.$eval(nextButtonselector, button => button.disabled);
+                console.log('Is button disabled?', isButtonDisabled);
+            } catch (error) {
+                console.error('An error occurred:', error.message);
+                retries--; // Decrement the retry count
+                if (retries <= 0) {
+                    console.log('Maximum retries reached. Exiting...');
+                    break; // Exit the loop if we've run out of retries
+                }
+                console.log(`Retrying... (${retries} retries left)`);
+                await page.waitForTimeout(10000); // Wait for 10 seconds before retrying
+            }
+        }
+    
         await browser.close();
+        //file write section
+        const jsonString = JSON.stringify(allQuotes, null, 2);
+        const path = "SkyScanner-comments.json";
+        fs.writeFile(path, jsonString, (err) => {
+            if (err) {
+              console.log("error: ", err);
+              return;
+            }
+            console.log(`data saved to ${path}`);
+        });
+          
+          // Database upload section
+await prisma.$transaction(async (tx) => {
+    try {
+        await Promise.all(
+            allQuotes.map(async (quote) => {
+                const where = {
+                    organization_id: "65c5a9760b5fff3be7a3afd3",
+                    storename: quote.hotel,
+                    rating: parseInt(quote.rating), // Convert rating to integer
+                };
+
+                // Adjust date format if necessary
+                const dateParts = quote.date.split('-');
+                const formattedDate = dateParts.map(part => {
+                    return part.length === 1 ? '0' + part : part;
+                }).join('-');
+
+                // Validate date format
+                const isValidDate = !isNaN(Date.parse(formattedDate));
+
+                if (isValidDate) {
+                    where.review_on = new Date(formattedDate).toISOString();
+                } else {
+                    console.error(`Invalid date format for quote: ${quote}`);
+                    return;
+                }
+
+                // Find if the review already exists in the database
+                const exist = await tx.review.findFirst({
+                    where: where,
+                });
+
+                if (!exist) {
+                    // If the review doesn't exist, create a new one
+                    await tx.review.create({
+                        data: {
+                            organization_id: "65c5a9760b5fff3be7a3afd3",
+                            storename: quote.hotel,
+                            topic: "",
+                            detail: quote.review,
+                            rating: parseInt(quote.rating), // Convert rating to integer
+                            review_on: where.review_on,
+                            language: quote.language,
+                            reference: "Skyscanner",
+                        },
+                    });
+                }
+            })
+        );
+        console.log("All quotes uploaded successfully.");
+    } catch (error) {
+        console.error("Error uploading quotes to the database:", error);
+    }
+});
+
+
+
+        
     } catch (error) {
         console.log("Error:", error);
     }
--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { franc } from "franc";
+import fs from "fs";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
 
 puppeteer.use(StealthPlugin());
 
@@ -12,7 +16,6 @@ async function scrollToBottom(page) {
         }
     });
 }
-
 (async () => {
     try {
         const browser = await puppeteer.launch({
@@ -28,8 +31,6 @@ async function scrollToBottom(page) {
         const buttonReviewSelector = '#app-root > div.MainContent_MainContent__section__Y2FjZ.SEOPageLayout_SEOPageLayout__hotelInformation__MDMxY > div > div.DesktopLayout_DesktopLayout__reviewWrap__ZTkwZ > div > div > section';
         await page.click(buttonReviewSelector);
         await scrollToBottom(page);
-        let isButtonDisabled = false;
-        let retries = 3; // Number of times to retry clicking the button
         
         let allQuotes = [];
            
@@ -72,12 +73,12 @@ async function scrollToBottom(page) {
                                 'ม.ค.': '01', 'ก.พ.': '02', 'มี.ค.': '03', 'เม.ย.': '04', 'พ.ค.': '05', 'มิ.ย.': '06',
                                 'ก.ค.': '07', 'ส.ค.': '08', 'ก.ย.': '09', 'ต.ค.': '10', 'พ.ย.': '11', 'ธ.ค.': '12'
                             }[month];
-                            return `${year}/${monthNumber}/${day}`;
+                            return `${year}-${monthNumber}-${day}`;
                         }
                         
                         boxCompress.push({
                             hotel: hotelName,
-                            review: reviewText,
+                            review: reviewText.replaceAll("\n",""),
                             // language: language,
                             rating: ratingText,
                             date: dateText,
@@ -91,73 +92,6 @@ async function scrollToBottom(page) {
                 });
                 
                 allQuotes = allQuotes.concat(quotes);
-        while (!isButtonDisabled && retries > 0) {
-            try {
-                const nextButtonselector = 'nav.BpkPagination_bpk-pagination__N2VhM > button:nth-child(3)';
-                let allQuotes = [];
-           
-        
-                const quotes = await page.evaluate(() => {
-                    const nameElement = document.querySelector('.DesktopLayout_DesktopLayout__nameStars__MGE5Y > h1');
-                    const hotelName = nameElement ? nameElement.textContent.trim() : null;
-                    
-                    const boxElement = document.querySelectorAll("div.ReviewCardItem_ReviewCardItem__YTU2Y ");
-                    
-                    const boxCompress = [];
-        
-                    boxElement.forEach(async element => {
-                        const reviewEl = element.querySelector("div.ReviewCardItem_ReviewCardItem__rightSection__YzZkZ > div.ReviewCardItem_ReviewCardItem__reviewContent__OWU1N > span");
-                        const reviewText = reviewEl ? reviewEl.innerText.trim() : "Review not found";
-                        
-                        const ratingEl = element.querySelector("span.BpkText_bpk-text__ZjI3M.BpkText_bpk-text--label-1__MWI4N.BpkRating_bpk-rating__value__YzhiN");
-                        const ratingText = ratingEl ? ratingEl.innerText.split('/')[0].trim() : "Rating not found";
-                        
-                        const dateEl = element.querySelector("div.ReviewCardItem_ReviewCardItem__guestInfo__YmE1Y > p.BpkText_bpk-text__ZjI3M.BpkText_bpk-text--caption__NzU1O.ReviewCardItem_ReviewCardItem__info__YTg5Z");
-                        const dateText = dateEl ? formatDate(dateEl.innerText) : "Date not found";
-                        console.log('Check Lang');
-                        // const language = franc(reviewText, { only: ['tha'] });
-                        console.log('Check Lang f');
-                    
-                    
-                        function formatDate(dateString) {
-                            const datePart = dateString.split(':')[1].trim();
-                            
-                            // Check if the date string matches the expected format
-                            const dateRegex = /^(\d{1,2}) (\S+) (\d{4})$/;
-                            if (!dateRegex.test(datePart)) {
-                                // If the date format doesn't match, return a default value
-                                return "Invalid date format";
-                            }
-                            
-                            // Parse the date and return it in YYYY/MM/DD format
-                            const [, day, month, year] = datePart.match(dateRegex);
-                            const monthNumber = {
-                                'ม.ค.': '01', 'ก.พ.': '02', 'มี.ค.': '03', 'เม.ย.': '04', 'พ.ค.': '05', 'มิ.ย.': '06',
-                                'ก.ค.': '07', 'ส.ค.': '08', 'ก.ย.': '09', 'ต.ค.': '10', 'พ.ย.': '11', 'ธ.ค.': '12'
-                            }[month];
-                            return `${year}/${monthNumber}/${day}`;
-                        }
-                        
-                        boxCompress.push({
-                            hotel: hotelName,
-                            review: reviewText,
-                            // language: language,
-                            rating: ratingText,
-                            date: dateText,
-                            reference: "Skyscanner",
-                        });
-                    });
-                    
-                    
-                    return boxCompress;
-                    
-                });
-                
-                allQuotes = allQuotes.concat(quotes);
-                
-                
-           
-        
         console.log('Data scraping complete.');
         console.log('Number of quotes:', allQuotes.length);
         allQuotes.forEach(obj => {
@@ -165,32 +99,77 @@ async function scrollToBottom(page) {
             obj.language = language;
             console.log(obj);
         });
-                
-                // Wait for the button to be available and click it. Increase the timeout if necessary.
-                await page.waitForSelector(nextButtonselector, { timeout: 60000 }); // 60 seconds
-                await page.click(nextButtonselector);
-    
-                // Wait for some time to allow for the state change to occur
-                await page.waitForTimeout(5000); // Adjust the time as needed
-    
-                // Check if the button is disabled
-                isButtonDisabled = await page.$eval(nextButtonselector, button => button.disabled);
-                console.log('Is button disabled?', isButtonDisabled);
-            } catch (error) {
-                console.error('An error occurred:', error.message);
-                retries--; // Decrement the retry count
-                if (retries <= 0) {
-                    console.log('Maximum retries reached. Exiting...');
-                    break; // Exit the loop if we've run out of retries
-                }
-                console.log(`Retrying... (${retries} retries left)`);
-                await page.waitForTimeout(10000); // Wait for 10 seconds before retrying
-            }
-        }
-    
+        
         await browser.close();
 
-        await browser.close();
+
+        //file write section
+        const jsonString = JSON.stringify(allQuotes, null, 2);
+        const path = "testDoc-comments.json";
+        fs.writeFile(path, jsonString, (err) => {
+            if (err) {
+              console.log("error: ", err);
+              return;
+            }
+            console.log(`data saved to ${path}`);
+          });
+          // Database upload section
+          await prisma.$transaction(
+            async (tx) => {
+                return await Promise.all(
+                    allQuotes.map(async (quote) => {
+                        const where = {
+                            organization_id: "65c5a9760b5fff3be7a3afd3",
+                            storename: quote.hotel,
+                            rating: parseInt(quote.rating), // Convert rating to integer
+                        };
+        
+                        // Adjust date format if necessary
+                        const dateParts = quote.date.split('-');
+                        const formattedDate = dateParts.map(part => {
+                            return part.length === 1 ? '0' + part : part;
+                        }).join('-');
+        
+                        // Validate date format
+                        const isValidDate = !isNaN(Date.parse(formattedDate));
+        
+                        if (isValidDate) {
+                            where.review_on = new Date(formattedDate).toISOString();
+                        } else {
+                            console.error(`Invalid date format for quote: ${quote}`);
+                            return;
+                        }
+        
+                        // Find if the review already exists in the database
+                        const exist = await tx.review.findFirst({
+                            where: where,
+                        });
+        
+                        if (!exist) {
+                            // If the review doesn't exist, create a new one
+                            await tx.review.create({
+                                data: {
+                                    organization_id: "65c5a9760b5fff3be7a3afd3",
+                                    storename: quote.hotel,
+                                    topic: "",
+                                    detail: quote.review,
+                                    rating: parseInt(quote.rating), // Convert rating to integer
+                                    review_on: where.review_on,
+                                    language: quote.language,
+                                    reference: "testdoc",
+                                    
+                                },
+                            });
+                        }
+                    })
+                );
+            },
+            { maxWait: 1000 * 60 * 10, timeout: 1000 * 60 * 15 }
+        );
+          
+
+
+
     } catch (error) {
         console.log("Error:", error);
     }
